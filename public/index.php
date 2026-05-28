a0c2c84dc46e65972bf5666897832605e19654f0
zura meskhi <meskhizuzu@gmail.com>
2026-05-28 18:52:08 +0400

    fix(settings): restore filament logo upload

--- public/index.php
+++ public/index.php
@@ -5,6 +5,25 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+$temporaryDirectory = __DIR__ . '/../storage/framework/tmp';
+
+if (! is_dir($temporaryDirectory)) {
+    mkdir($temporaryDirectory, 0777, true);
+}
+
+putenv("TMP={$temporaryDirectory}");
+putenv("TEMP={$temporaryDirectory}");
+putenv("TMPDIR={$temporaryDirectory}");
+
+$_ENV['TMP'] = $temporaryDirectory;
+$_ENV['TEMP'] = $temporaryDirectory;
+$_ENV['TMPDIR'] = $temporaryDirectory;
+$_SERVER['TMP'] = $temporaryDirectory;
+$_SERVER['TEMP'] = $temporaryDirectory;
+$_SERVER['TMPDIR'] = $temporaryDirectory;
+
+ini_set('sys_temp_dir', $temporaryDirectory);
+
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,7 @@
 <?php
 
-use App\Http\Controllers\CompanyLogoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::middleware('auth')->group(function (): void {
-    Route::post('/admin/company-settings/logo', [CompanyLogoController::class, 'store'])
-        ->name('admin.company-settings.logo.store');
-
-    Route::delete('/admin/company-settings/logo', [CompanyLogoController::class, 'destroy'])
-        ->name('admin.company-settings.logo.destroy');
 });
