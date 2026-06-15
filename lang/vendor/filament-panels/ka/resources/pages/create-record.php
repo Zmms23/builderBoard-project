e9da140bb434d660d20fa12ddff4919e2715f5e9
zura meskhi <meskhizuzu@gmail.com>
2026-06-15 12:30:51 +0400

    fix(translations): correct Georgian create labels

---
+++ lang/vendor/filament-panels/ka/resources/pages/create-record.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+
+    'title' => 'შექმენით :label',
+
+    'breadcrumb' => 'შექმნა',
+
+    'form' => [
+
+        'actions' => [
+
+            'cancel' => [
+                'label' => 'გაუქმება',
+            ],
+
+            'create' => [
+                'label' => 'შექმნა',
+            ],
+
+            'create_another' => [
+                'label' => 'შექმნა და ახლის შექმნა',
+            ],
+
+        ],
+
+    ],
+
+    'notifications' => [
+
+        'created' => [
+            'title' => 'შექმნილია',
+        ],
+
+    ],
+
+];
